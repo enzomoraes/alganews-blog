66d3b1b29f303ae315b30bc2ffea7f1faa9278bc
enzomoraes <enzomoraes12@hotmail.com>
2022-12-07 21:17:58 -0400

    chore: setup project, styled-components and babel plugin

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { light } from '../styles/theme';
+import GlobalStyles from '../styles/globalStyles';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={light}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
