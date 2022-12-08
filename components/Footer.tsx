b5675ac548bd46736da9760a6ec0eaa52d9c5a78
enzomoraes <enzomoraes12@hotmail.com>
2022-12-07 21:55:01 -0400

    feature: footer header and content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,33 +1,39 @@
 import { transparentize } from 'polished';
 import styled from 'styled-components';
-import { HEADER_HEIGHT } from '../_constants';
+import { FOOTER_HEIGHT } from '../_constants';
 import Logo from './Logo';
 
-export default function Header() {
+export default function Footer() {
   return (
     <Wrapper>
       <Container>
         <Logo />
-        <span>navbar</span>
+        <Credits>todos os direitos reservados</Credits>
       </Container>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.header`
+const Wrapper = styled.footer`
   background-color: ${p => p.theme.activeElementBackground};
   color: ${p => p.theme.activeElementForeground};
-  box-shadow: 0 3px 10px ${p => transparentize(0.9, p.theme.pageForeground)};
 
   width: 100%;
-  height: ${HEADER_HEIGHT}px;
+  height: ${FOOTER_HEIGHT}px;
 `;
 
 const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
   max-width: 848px;
   margin: auto;
   height: 100%;
+`;
+
+const Credits = styled.p`
+  font-size: 18px;
+  color: ${p => transparentize(0.6, p.theme.activeElementForeground)};
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Header></Header>
         <h1 className={styles.title}>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
@@ -55,19 +54,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
